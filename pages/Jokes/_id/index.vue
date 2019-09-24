e1b7a3883a03df3be42e2d5d4c1b2d7792e06675
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2019-09-24 20:31:09 +0500

    second commit

---
+++ pages/Jokes/_id/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <nuxt-link to="/jokes">
+      Back To Jokes
+    </nuxt-link>
+    <h2>
+      {{ joke }}
+    </h2>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      joke: {}
+    }
+  },
+  async created () {
+    const config = {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }
+    const res = await axios.get(`https://icanhazdadjoke.com/j/${this.$route.params.id}`, config)
+    this.joke = res.data.joke
+  },
+  head () {
+    return {
+      title: this.joke,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped></style>
